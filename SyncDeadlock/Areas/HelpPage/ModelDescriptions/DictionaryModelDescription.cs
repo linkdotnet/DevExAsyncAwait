f9c91283209da5d9eeef364ec5d640752f73b4ea
Steven Giesel <stgiesel35@gmail.com>
2021-07-24 17:16:49 +0200

    Removed unused stuff

--- SyncDeadlock/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SyncDeadlock.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SyncDeadlock/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SyncDeadlock.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
